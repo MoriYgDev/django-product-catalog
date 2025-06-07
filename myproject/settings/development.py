30279aee20e291529a28fc7d74b54a25660c4097
Morteza Yaghoobi <morteza.yaghoobi1375@gmail.com>
2025-06-07 11:37:05 +0330

    improved scurity and scalabilaty and some minor UI changes

--- myproject/settings/development.py
+++ myproject/settings/development.py
@@ -4,7 +4,7 @@ import os # Make sure this is here
 
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 DEBUG = True  # <--- Is this definitely True?
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.2.115' , 'localhost', '127.0.0.1']
 
 
 DATABASES = {
